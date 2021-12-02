4e9927f8797fa863e64888da90c127c4c7a60ab9
smhrd <smhrd@DESKTOP-PFJOTFT>
2021-12-02 16:14:59 +0900

    환경구축 설명추가

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	      // 3. Encoding 방식(UTF-8)
 	      // 이 내용은 팀 내 한분만 작성해주세요!
 	      // 혹시 충돌을 경험해보면 싶으시다면 작성해보세요!
-	      	
+	      //	
 		System.out.println("First Code");
 		
 	}
